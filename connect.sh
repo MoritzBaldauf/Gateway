bc191c77cd0c44312051c21e2620454b9176399b
MoritzBaldauf <moritz.baldauf24@gmail.com>
2021-03-12 07:27:22 +0000

    Scripts

---
+++ connect.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo pppd call gprs &
+echo "Verbindung aufgebaut"
+
---
+++ disconnect.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Script um Internetverbindung zubeenden
+sudo killall pppd 
+echo Internetverbindung wurde beendet 
