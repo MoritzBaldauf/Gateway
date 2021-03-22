a5edcf78453c4a78b05ac7ddc73b681dcbde9eb1
MoritzBaldauf <78202751+MoritzBaldauf@users.noreply.github.com>
2021-03-22 19:27:02 +0100

    Update disconnect.sh

--- disconnect.sh
+++ disconnect.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #**************************************************************************
-#*	Date:		12.03.2021							 																	    *
-#*	Name:		Moritz Baldauf													   										*
-#*	Hardware:	Raspberry PI 3B+                                            *
-#*            RAK2013                                                     *
-#*            RAK2245                                                     *
-#*	Function: Disconnects the ppp0 connection and                         *
-#*            returns a Message if successful. 							              *
-#*													   																						*	  
+# 	Date:		12.03.2021							 																	    
+#	  Name:		Moritz Baldauf													   										
+#	  Hardware:	Raspberry PI 3B+                                            
+#             RAK2013                                                     
+#             RAK2245                                                     
+#	  Function: Disconnects the ppp0 connection and                         
+#             returns a Message if successful. 							             
+#													   																							  
 #**************************************************************************
 sudo killall pppd 
 echo Connection closed 
