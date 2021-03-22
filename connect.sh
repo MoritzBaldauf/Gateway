6c430d017d2052712a4d08de37babc7c9b4f88ee
MoritzBaldauf <78202751+MoritzBaldauf@users.noreply.github.com>
2021-03-22 19:24:42 +0100

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#**************************************************************************
+#*	Date:		12.03.2021							 																	    *
+#*	Name:		Moritz Baldauf													   										*
+#*	Hardware:	Raspberry PI 3B+                                            *
+#*            RAK2013                                                     *
+#*            RAK2245                                                     *
+#*	Function: Establishes a connection trough sudo pppd call gprs and     *
+#*            returns a Message if connection is successful. 							*
+#*													   																						*	  
+#**************************************************************************
+
 sudo pppd call gprs &
 echo "Connection OK"
 
