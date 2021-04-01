f711ad23214feea56bc46cccf6340270b9f1da3c
MoritzBaldauf <78202751+MoritzBaldauf@users.noreply.github.com>
2021-04-01 15:08:51 +0200

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-
-
+#**************************************************************************
+#*	Date:		18.03.2021							 																	    
+#*	Name:		Moritz Baldauf													   										
+#*	Hardware:	Raspberry Pi 3B+                                            
+#*            RAK2013                                                     
+#*            RAK2245  
+#*
+#*	Function:Tests the speed of the Connection through ppp0
+#*           If the installation for the file is still needed
+#*           Check https://sbcguides.com/internet-speed-test-on-raspberry-pi/
+#*													   																							  
+#**************************************************************************
+fast
