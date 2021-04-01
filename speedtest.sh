9a161f654f7e3895fecffd3f572daf2da5a58278
MoritzBaldauf <78202751+MoritzBaldauf@users.noreply.github.com>
2021-04-01 17:01:03 +0200

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #*****************************************************************************
-#*     Date:		18.03.2021							 																	    
-#*     Name:		Moritz Baldauf													   										
-#*     Hardware:	Raspberry Pi 3B+                                            
-#*               RAK2013                                                     
-#*               RAK2245  
+#* Date: 18.03.2021
+#* Name: Moritz Baldauf
+#* Hardware: Raspberry Pi 3B+
+#*           RAK2013
+#*           RAK2245
 #*
-#*     Function: Tests the speed of the Connection through ppp0
-#*               If the installation for the file is still needed
-#*               Check https://sbcguides.com/internet-speed-test-on-raspberry-pi/
-#*													   																							  
+#* Function: Tests the speed of the Connection through ppp0
+#*           If the installation for the file is still needed
+#*   Check https://sbcguides.com/internet-speed-test-on-raspberry-pi/
 #*****************************************************************************
-#Speedtest
-fast
+
+if ! [ -d "/home/pi/fast_linux_arm" ]; then
+#Check if the files are installed then run speed test
+        fast
+else
+        echo "Please install the missing files"
+#If files are not installed give Error message
+fi
