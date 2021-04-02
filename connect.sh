59ac975ea3ec2dd266904568abc9627d7a02d380
MoritzBaldauf <78202751+MoritzBaldauf@users.noreply.github.com>
2021-04-02 09:22:27 +0200

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 #**************************************************************************
-#*      Date:           18.03.2021							 																	    
-#*      Name:           Moritz Baldauf													   										
-#*      Hardware:       Raspberry Pi 3B+                                            
-#*                      RAK2013                                                     
-#*                      RAK2245  
+#* Date: 18.03.2021
+#* Name: Moritz Baldauf
+#* Hardware: Raspberry Pi 3B+
+#*           RAK2013
+#*           RAK2245
 #*
-#*      Function: Establishes a connection trough sudo pppd call gprs.
-#*                The Message log is written in log.txt. 
-#*                If connection is built successful. 							
-#*													   																							  
+#* Function: Establishes a connection trough sudo systemctl start rak-pppd.
+#* Then we check if pppd is running, if so the if so the  connection
+#* was built successfully.
 #**************************************************************************
-
-sudo pppd call gprs > log.txt &
-#The Connection is established the message is written in log.txt
-sleep 5
-#Wait for 5 Seconds to make sure the connection is finished
-status=$(grep -oh "status\s=\s0x[[:digit:]]" log.txt)
-#Check if there is a status=s0x0 in log.txt and if so, we write it in $status
-if [[ -z $status ]]; then 
-#Check if there is something written in $status
-#If it is empty the connection went wrong
-        echo "Fehler beim Verbindungsaufbau"
-        echo "1"
-else
-#If there is something written in $status then the connection is successful
-        echo "Verbindung aufgebaut"
+sudo systemctl start rak-pppd
+#The Connection is established
+sleep 1
+#Wait for 1 Seconds to make sure the connection is finished
+status=$(ps -A | grep pppd)
+#Check if pppd is runnging
+#and if so wirte it in $status
+if [[ -z $status ]]; then
+#Check if there is something written in $status If it is empty the
+#connection was no established wrong
+        echo "Connection Error"
+         echo "1"
+ else
+#If there is something written in $status then the connection was
+#successful
+        echo "Connection OK"
         echo "0"
 fi
+
 
 
 
