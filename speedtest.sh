005d101e1a748796a881f521839797187d68b96c
MoritzBaldauf <78202751+MoritzBaldauf@users.noreply.github.com>
2021-04-02 11:47:05 +0200

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -7,14 +7,22 @@
 #*           RAK2245
 #*
 #* Function: Tests the speed of the Connection through ppp0
-#*           If the installation for the file is still needed
+#*           If the installation for the files went wrong
 #*   Check https://sbcguides.com/internet-speed-test-on-raspberry-pi/
 #*****************************************************************************
 
 if ! [ -d "/home/pi/fast_linux_arm" ]; then
 #Check if the files are installed then run speed test
         fast
+
 else
-        echo "Please install the missing files"
+        echo "Installing missing files"
 #If files are not installed give Error message
+#and install the Files
+        wget https://github.com/ddo/fast/releases/download/v0.0.4/fast_linux_arm
+#get files from Github
+        sudo install fast_linux_amd64 /usr/local/bin/fast
+#install the Files
+        echo "Files are now installed please run Speedtest again"
+
 fi
