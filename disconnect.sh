2b0d97b813e5c99fc26d86b3798e5998379059ce
MoritzBaldauf <78202751+MoritzBaldauf@users.noreply.github.com>
2021-04-02 09:29:32 +0200

    Update disconnect.sh

--- disconnect.sh
+++ disconnect.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 #**************************************************************************
-#   Date:       12.03.2021							 																	    
-#   Name:       Moritz Baldauf													   										
-#   Hardware:   Raspberry PI 3B+                                            
-#               RAK2013                                                     
-#               RAK2245  
-#
-#   Function: Disconnects the ppp0 connection and                         
-#             returns a Message if successful. 							             
-#													   																							  
+#*   Date:      12.03.2021							 																	    
+#*   Name:      Moritz Baldauf													   										
+#*   Hardware:  Raspberry PI 3B+                                            
+#*              RAK2013                                                     
+#*              RAK2245  
+#*
+#*   Function:  Disconnects the ppp0 connection and                         
+#*              returns a Message if successful. 							             
+#*													   																							  
 #**************************************************************************
 
 statusvor=$(ps -A | grep pppd)
+#Checks if there is a ppp connection running
 if [[ -z $statusvor ]]; then
-        echo "Keine Verbindung vorhanden"
+#If there is no Connection return an Error
+        echo "Could not find any connection"
         exit;
 else
+#Else close the connection
         sudo systemctl stop rak-pppd
         sleep 1
 fi
 
 statusnach=$(ps -A | grep pppd)
+#Check if there is still a Connection running
 if [[ -z $statusnach ]]; then
-        echo "Internetverbindung wurde beendet"
+#If not the task was succesfull
+        echo "Interent Connection has been stoped"
         echo "0"
 else
-        echo "Internetverbindung konnte nicht beendet werden"
+#If ther is still a Connection return a Error Message  
+        echo "Error when trying to stop Internet Connection"
         echo "1"
 fi
